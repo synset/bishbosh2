9fc206c4f382230c5b8e8ebcfca6374a5b508735
synset <synnove.settman@yahoo.com>
2020-04-24 16:23:05 +0200

    Add project files.

---
+++ bishbosh2/bishbosh.js
@@ -0,0 +1,16 @@
+﻿function runBishBosh() {
+
+    let bish = document.getElementById("bishNumber").value;
+    let bosh = document.getElementById("boshNumber").value;
+    let number = document.getElementById("lengthNumber").value;
+
+    for (let i = 1; i <= number; i++) {
+        let results = "";
+        if (i % bish == 0 && i % bosh == 0) results = "Bish-Bosh";
+        else if (i % bish == 0) results = "Bish";
+        else if (i % bosh == 0) results = "Bosh";
+        else results = i;
+
+        document.getElementById("displayResults").innerHTML += results + '<br />';
+    }
+}
